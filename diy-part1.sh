220bc48486d8458e8279b9cb82a54a00d9547381
shine85 <73932625+shine85@users.noreply.github.com>
2023-09-27 09:11:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,8 @@
 # Use OpenWrt 19.07.7 release
 # git checkout v18.06.9
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/gl-inet/parental-control.git package/parental-control
 git clone https://github.com/mslovecc/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/mslovecc/luci-app-argon-config.git package/luci-app-argon-config
 mkdir package/ming
